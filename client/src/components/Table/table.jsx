242d18da5e3c69b80b208005d477a258617c75e4
VasiliyKozlov <vasdram@yandex.ru>
2020-09-10 14:52:59 +0300

    fix table fields

--- client/src/components/Table/table.jsx
+++ client/src/components/Table/table.jsx
@@ -9,33 +9,35 @@ export const Table = ({data}) => {
              <thead>
                 <tr>
                 <th scope="col">#</th>
-                <th scope="col">Вид Оплаты</th>
-                <th scope="col">Вид Платежа</th>
-                <th scope="col">Дата</th>
-                <th scope="col">Дата Списано</th>
-                <th scope="col">Код</th>
-                <th scope="col">Назначение Платежа</th>
-                <th scope="col">Номер</th>
-                <th scope="col">ОКАТО</th>
-                <th scope="col">Очередность</th>
-                <th scope="col">Плательщик БИК</th>
-                <th scope="col">Плательщик Банк1</th>
+                <th scope="col">Дата и время</th>
+                <th scope="col">Номер документа</th>
+                <th scope="col">Приход(+)</th>
+                <th scope="col">Контраагент</th>
+                <th scope="col">Расход(-)</th>
+                <th scope="col">Наименование платежа</th>
+                <th scope="col">Признак НДС</th>
+                <th scope="col">Коммисия %</th>
+                <th scope="col">Сумма коммисии</th>
+                <th scope="col">Категории платежа</th>
+                <th scope="col">Оператор</th>
+                <th scope="col">Комментарий</th>
                 </tr>
             </thead>
             {data.map((row, idx) => {
                 return <tr>
                     <th scope="row">{idx++}</th>
-                    <td contenteditable="true">{row["ВидОплаты"]}</td>
-                    <td contenteditable="true">{row["ВидПлатежа"]}</td>
-                    <td contenteditable="true">{row["Дата"]}</td>
-                    <td contenteditable="true">{row["ДатаСписано"]}</td>
-                    <td contenteditable="true">{row["Код"]}</td>
-                    <td contenteditable="true" className={styles.pointCell}>{row["НазначениеПлатежа"]}</td>
+                    <td contenteditable="true">10.09.2020</td>
                     <td contenteditable="true">{row["Номер"]}</td>
-                    <td contenteditable="true">{row["ОКАТО"]}</td>
-                    <td contenteditable="true">{row["Очередность"]}</td>
-                    <td contenteditable="true">{row["ПлательщикБИК"]}</td>
-                    <td contenteditable="true">{row["ПлательщикБанк1"]}</td>
+                    <td contenteditable="true">{row["ВсегоПоступило"]}</td>
+                    <td contenteditable="true">{row["Плательщик"]}</td>
+                    <td contenteditable="true">{row["ВсегоСписано"]}</td>
+                    <td contenteditable="true">{row["НазначениеПлатежа"]}</td>
+                    <td contenteditable="true">--//--</td>
+                    <td contenteditable="true">--//--</td>
+                    <td contenteditable="true">--//--</td>
+                    <td contenteditable="true">--//--</td>
+                    <td contenteditable="true">--//--</td>
+                    <td contenteditable="true">--//--</td>
                 </tr>
             })}
         </table>
